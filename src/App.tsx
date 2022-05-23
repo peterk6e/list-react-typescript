be5dc9621181b73f5019cdd21b8be8f95e0600b6
pierrek6e <pierrecass75@gmail.com>
2022-05-23 09:36:23 +0700

    basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { FC } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: FC = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="input-container">
+          <input className="task-input" type="text" placeholder="Task..." />
+          <input className="days-input" type="number" placeholder="Days..." />
+        </div>
+        <button> + Add </button>
+      </div>
+      <div className="list"></div>
+    </div>
+  );
+};
 
 export default App;
