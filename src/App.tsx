d3f4ab13035573a3c3a8268ae77fe74d086200df
pierrek6e <pierrecass75@gmail.com>
2022-05-23 10:34:33 +0700

    handle states and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React, { FC } from "react";
+import React, { FC, ChangeEvent, useState } from "react";
+import { ITask } from "./Interfaces";
 import "./App.css";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [days, setDays] = useState<number>(0);
+  const [list, setList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDays(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskText: task, days: days };
+    setList([...list, newTask]);
+  };
+
   return (
     <div className="App">
       <div className="header">
@@ -10,14 +28,18 @@ const App: FC = () => {
             className="task-input"
             type="text"
             placeholder="What do you have to do?"
+            name="task"
+            onChange={handleChange}
           />
           <input
             className="days-input"
             type="number"
             placeholder="in how many days?"
+            name="days"
+            onChange={handleChange}
           />
         </div>
-        <button> + Add </button>
+        <button onClick={addTask}> + Add </button>
       </div>
       <div className="list"></div>
     </div>
---
+++ src/Interfaces.ts
@@ -0,0 +1,4 @@
+export interface ITask {
+  taskText: string;
+  days: number;
+}
